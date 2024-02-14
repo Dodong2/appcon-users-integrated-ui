5d0483c1668e95969db92058b2dcdddef5cdbaf2
unknown <carlseighartaliode@gmail.com>
2024-02-14 22:05:54 +0800

    added some responsiveness and modal but no style

--- src/components/Flow2/Flow2.jsx
+++ src/components/Flow2/Flow2.jsx
@@ -1,11 +1,20 @@
 import style from './Flow2.module.css'
 import logo from '../images/Temologo.svg'
+import location from '../images/location.svg'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react'
 const Flow1 = () => {
+
+  const [isOpenModal, setOpenModal] = useState(false)
+
+  const tggleModal = () => {
+    setOpenModal(!isOpenModal) 
+  }
+
   return (
     <>
     {/*container*/}
     <div className={style.container}>
-    <div className={style.card}>
     {/*LOGO*/}
       <div className={style['center-contents']}>
         <img src={logo} alt='logo'/>
@@ -21,10 +30,30 @@ const Flow1 = () => {
       </div>
       {/*Button*/}
         <div className={style.btn}>
-        <button>Find Nearest Hospital</button>
-        </div>
+        <button onClick={tggleModal} type="button">Find Nearest Hospital</button>
         </div>
     </div>
+
+    <div className={style.alert}>
+      <img src={location} />
+
+      {isOpenModal && (
+        <div>
+          <main className={`${style['main-container']} ${isOpenModal ? style['open-modal'] : ''}`} onClick={tggleModal}>
+            <button onClick={tggleModal} type="button">Open Modal</button>
+          </main>
+          <div className={`${style['modal-container']} ${isOpenModal ? style['open-modal'] : ''}`} onClick={tggleModal}></div>
+          <div className={`${style['modal-window']} ${isOpenModal ? style['open-modal'] : ''}`}>
+            <h2>Modal Heading</h2>
+            <p>
+              Modal Description: Short paragraph about the content related to Modal. You can add UI elements or HTML components.
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+
+
     </>
   )
 }
