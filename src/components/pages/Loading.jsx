4c425edddfe2dc4fd6b1b36e093e4be9d3186797
unknown <carlseighartaliode@gmail.com>
2024-02-19 23:26:11 +0800

    update ui

--- src/components/pages/Loading.jsx
+++ src/components/pages/Loading.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react'
-import logo from '../images/Temologo.svg'
+import loadlayer1 from "../images/load1-layer.svg";
+import loadlayer2 from "../images/load2-layer.svg";
+import loadlayer3 from "../images/load3-layer.svg";
+import loadlayer4 from "../images/load4-layer.svg";
+
 
 function App() {
 
@@ -18,11 +22,18 @@ function App() {
     <>
 {/* Loading Page */}
       <div className={`loading-container ${isLoading ? 'visible' : 'hidden'}`}>
-        <div className={`first-page ${isLoading ? 'visible' : 'hidden'}`}>
+        <div className={`${isLoading ? 'visible' : 'hidden'}`}>
           <div className="loading-logo">
             {/*LOGO*/}
             <div className="logo">
-              <img src={logo} alt='logo'/>
+            <div className="image-stack">
+            <div className="layer-stack">
+              <img src={loadlayer1} alt="" />
+              <img src={loadlayer2} alt="" />
+              <img src={loadlayer3} alt="" />
+              <img src={loadlayer4} alt="" />
+            </div>
+          </div>
             </div>
           </div>
         </div>
--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,33 +1,36 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Load from './Loading';
-import logo from '../images/Temologo.svg';
-import location from '../images/location.svg';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Load from "./Loading";
+import layer1 from "../images/layer1.svg";
+import layer2 from "../images/layer2.svg";
+import layer3 from "../images/layer3.svg";
+import layer4 from "../images/layer4.svg";
+import location from "../images/location.svg";
 
 const Home = () => {
   //useState Sections
   const [isOpenModal, setOpenModal] = useState(false);
   const [isGettingLocation, setGettingLocation] = useState(false);
 
-//for title
+  //for title
   const navigate = useNavigate();
   useEffect(() => {
-    document.title = 'HEAN';
+    document.title = "HEAN";
   }, []);
 
-//for Show allow access
+  //for Show allow access
   const tggleModal = () => {
     setOpenModal(!isOpenModal);
   };
-  
-//for allow location
+
+  //for allow location
   const allowLocation = () => {
     setGettingLocation(true);
     setTimeout(() => {
       navigator.geolocation.getCurrentPosition((position) => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-        console.log('Latitude:', latitude, 'Longitude:', longitude);
+        console.log("Latitude:", latitude, "Longitude:", longitude);
         navigate(`/hospitals?lat=${latitude}&long=${longitude}`);
         setGettingLocation(false);
       });
@@ -39,40 +42,56 @@ const Home = () => {
       {/* Splash Screen */}
       <Load />
       {isOpenModal ? (
-        <div className='container'>
-         {/* if modal is open magpapakita to once na ma-click si button na find nearest.....*/}
+        <div className="container">
+          {/* if modal is open magpapakita to once na ma-click si button na find nearest.....*/}
           <div className="center-contents">
-          <div>
-            <img src={location} alt='logo' />
-            <h1>Access your location</h1>
-            <p>We need your permission to access your location.</p>
+            <div>
+              <img src={location} alt="logo" />
+              <h1>Access your location</h1>
+              <p>We need your permission to access your location.</p>
             </div>
           </div>
           <div className="btn">
-            <button 
-              onClick={allowLocation} 
+            <button
+              onClick={allowLocation}
               type="button"
               disabled={isGettingLocation}
             >
-              {isGettingLocation ? 'Getting your location...' : 'Allow Location'}
+              {isGettingLocation
+                ? "Getting your location..."
+                : "Allow Location"}
             </button>
           </div>
         </div>
       ) : (
-        <div className='container'>
-         {/* unang makikita after ng loading */}
+        <div className="container">
+          {/* unang makikita after ng loading */}
           <div className="center-contents">
-          <div>
-            <img src={logo} alt='logo' />
-            <h1>HEAN</h1>
-            <p>Health Alert Network</p>
+            <div>
+            <div className="image-stack">
+            <div className="layer-stack">
+              <img src={layer1} alt="" />
+              <img src={layer2} alt="" />
+              <img src={layer3} alt="" />
+              <img src={layer4} alt="" />
             </div>
-            
+          </div>
+              <h1>HEAN</h1>
+              <p>Health Alert Network</p>
+            </div>
           </div>
           <div className="btn">
-              <p>Your guide to the nearest hospital that could cater your emergency needs.</p>
-              <p>Just click the button below 💙</p>
-            <button onClick={tggleModal} type="button">Find Nearest Hospital</button>
+          <div>
+                <p>
+                  Your guide to the nearest hospital that could cater your
+                  emergency needs.
+                </p>
+                <p>Just click the button below 💙</p>
+              </div>
+            <button onClick={tggleModal} type="button">
+              Find Nearest Hospital
+            </button>
+            
           </div>
         </div>
       )}
