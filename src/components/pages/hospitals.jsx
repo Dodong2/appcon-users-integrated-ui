bcb1c8cb8ca81ca92ea5d12bb82d33d140a13b81
unknown <carlseighartaliode@gmail.com>
2024-02-26 23:07:25 +0800

    update hsopitals ui

--- src/components/pages/hospitals.jsx
+++ src/components/pages/hospitals.jsx
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 import Load from "./Loading";
 import "bootstrap/dist/css/bootstrap.css";
 import Host from "../images/Host.png";
-import Loc from "../images/loc.svg"
-import bell from "../images/bell.svg"
+import Loc from "../images/loc.svg";
+import bell from "../images/bell.svg";
 
 const Hospitals = () => {
   useEffect(() => {
@@ -17,6 +17,7 @@ const Hospitals = () => {
   return (
     <>
       <Load />
+      {/* Hostpital 1 */}
       <div className="box">
         <div className="card">
           <img src={Host} className="card-img-top" alt="..." />
@@ -32,21 +33,90 @@ const Hospitals = () => {
             </div>
           </div>
           <div className="card-body">
-          
-          <div className="btn1">
-            <button><img src={Loc}/></button>
+            <div className="btn1">
+              <button>
+                <img src={Loc} />
+              </button>
             </div>
             <div className="btn2">
-            <button>Ambulance</button>
+              <button>Call Ambulance</button>
             </div>
             <div className="btn3">
-            <button><img src={bell}/> <p>Emergency</p></button>
+              <button>
+                <img src={bell} />
+                <p>Emergency</p>
+              </button>
             </div>
-          
           </div>
         </div>
       </div>
-    </>
+      {/* Hostpital 2*/}
+      <div className="box">
+        <div className="card">
+          <img src={Host} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <div className="txt-left">
+              <h1>Hostpital 2</h1>
+              <p>CMC, Real, Calamba, 4027 Laguna</p>
+            </div>
+            <div className="txt-right">
+              <p>
+                <span>22</span> Available Beds
+              </p>
+            </div>
+          </div>
+          <div className="card-body">
+            <div className="btn1">
+              <button>
+                <img src={Loc} />
+              </button>
+            </div>
+            <div className="btn2">
+              <button>Call Ambulance</button>
+            </div>
+            <div className="btn3">
+              <button>
+                <img src={bell}/>
+                <p>Emergency</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Hostpital 3 */}
+      <div className="box">
+        <div className="card">
+          <img src={Host} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <div className="txt-left">
+              <h1>Hostpital 3</h1>
+              <p>CMC, Real, Calamba, 4027 Laguna</p>
+            </div>
+            <div className="txt-right">
+              <p>
+                <span>22</span> Available Beds
+              </p>
+            </div>
+          </div>
+          <div className="card-body">
+            <div className="btn1">
+              <button>
+                <img src={Loc} />
+              </button>
+            </div>
+            <div className="btn2">
+              <button>Call Ambulance</button>
+            </div>
+            <div className="btn3">
+              <button>
+                <img src={bell}/>
+                <p>Emergency</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
   );
 };
 
